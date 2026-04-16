7a60d9e3256c449dbdb08d96972406b64e5795f9
Jared Nepomuceno <JaredNepomuceno519@gmail.com>
2026-04-16 01:16:58 -0700

    finished?

--- shift_register.sv
+++ shift_register.sv
@@ -1,21 +1,24 @@
 module shift_register #(parameter DIRECTION = 0) 
 (
-    input  logic        clk,
-    input  logic        rst,
-    input  logic        data,
-    input  logic        wr_en,
-    output logic [31:0] out
+    input clk,
+    input rst,
+    input data,
+    input wr_en,
+    output[31:0] out
 );
 
-    always_ff @(posedge clk) begin
+    reg [31:0] register;
+    assign out = register;
+
+    always @(posedge clk) begin
         if (rst) begin
-            out <= 32'b0;
+            register <= 32'b0;
         end else if (wr_en) begin
-            if (DIRECTION) begin
-                out <= {data, out[31:1]}; // go the right
-            end else begin
-                out <= {out[30:0], data}; //go the left
-            end
+            if (DIRECTION) 
+                register <= {data, out[31:1]}; // go the right
+            else
+                register <= {out[30:0], data}; //go the left
+            
         end
     end
 
